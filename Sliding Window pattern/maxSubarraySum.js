4966b69e10799a2b395740e4ae341687322d93e7
Revanth Pinninti <70140234+revanthpinninti@users.noreply.github.com>
2021-04-22 12:10:48 +0530

    Updated time and space complexities
    Moved to a folder

--- Sliding Window pattern/maxSubarraySum.js
+++ Sliding Window pattern/maxSubarraySum.js
@@ -23,3 +23,7 @@ console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)); //39
 console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2));   //5
 console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2));    //5
 console.log(maxSubarraySum([2,3], 3));  //null
+
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
