8c34aaf1bf07d8f2efa4090e7192bf3e27396e7d
Revanth Pinninti <70140234+revanthpinninti@users.noreply.github.com>
2021-04-22 11:03:38 +0530

    Updated time and space complexities
    Moved to a folder

--- Multiple Pointers/countUniqueValues.js
+++ Multiple Pointers/countUniqueValues.js
@@ -1,3 +1,4 @@
+// This problem is solved using Multiple Pointers pattern
 function countUniqueValues(arr) {
   if(arr.length === 0)
     return 0;
@@ -20,3 +21,6 @@ console.log(countUniqueValues([1,1,1,1,1,2]));  //2
 console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));  //7
 console.log(countUniqueValues([]));  //0
 console.log(countUniqueValues([-2,-1,-1,0,1]));  //4
+
+//Time Complexity - O(n)
+// Space Complexity - O(1)
