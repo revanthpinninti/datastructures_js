6a14745d086fdd44d79cd973fc903344f4f817ea
Revanth Pinninti <70140234+revanthpinninti@users.noreply.github.com>
2021-04-22 12:13:45 +0530

    Updated time and space complexities
    Moved to a folder

--- Frequency Counter Pattern/sameFrequency.js
+++ Frequency Counter Pattern/sameFrequency.js
@@ -32,3 +32,6 @@ function sameFrequency(num1, num2){
   console.log(sameFrequency(34, 14));   //false
   console.log(sameFrequency(3589578, 5879385)); //true
   console.log(sameFrequency(22, 222));  //false
+
+// Time Complexity - O(n)
+// Space Complexity - O(n)
