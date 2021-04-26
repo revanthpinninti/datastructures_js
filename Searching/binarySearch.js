3e9e33944b3a98e4fdb13ccc4a0d893f8e4a4f62
Revanth Pinninti <70140234+revanthpinninti@users.noreply.github.com>
2021-04-26 14:31:37 +0530

    Linear Search and Binary Search

---
+++ Searching/binarySearch.js
@@ -0,0 +1,25 @@
+function binarySearch(arr, val){
+    let left =0, right = arr.length-1;
+    let middle = Math.floor((left+right)/2);
+
+    while(left<=right) {
+        if(val === arr[middle]) return middle;
+        else if(val < arr[middle]){
+            right = middle;
+            middle = Math.floor((left+right)/2);
+        }
+        else if(val > arr[middle]){
+            left = middle+1;
+            middle = Math.floor((left+right)/2);
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1,2,3,4,5], 2));  //1
+console.log(binarySearch([1,2,3,4,5], 3));  //2
+console.log(binarySearch([1,2,3,4,5], 5));  //4
+console.log(binarySearch([1,2,3,4,5], 6));  //-1
+console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 10));  //2
+console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 95));  //16
+console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 100));  //-1
---
+++ Searching/linearSearch.js
@@ -0,0 +1,17 @@
+function linearSearch(arr, key){
+ for(let i=0; i<arr.length;i++) {
+     if(arr[i] === key)
+        return i;
+ }
+    return -1;
+}
+
+console.log(linearSearch([10,15,20,25,30], 15));    //1
+console.log(linearSearch([9,8,7,6,5,4,3,2,1,0], 4));    //5
+console.log(linearSearch([100], 100));    //0
+console.log(linearSearch([1,2,3,4,5], 6));    //-1
+console.log(linearSearch([9,8,,7,6,5,4,3,2,1,0], 10));    //-1
+console.log(linearSearch([100], 200));    //-1
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
