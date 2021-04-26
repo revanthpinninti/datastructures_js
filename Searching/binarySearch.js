5471ec58edd22e45ca23b99d21e4c4f8a2e1b907
Revanth Pinninti <70140234+revanthpinninti@users.noreply.github.com>
2021-04-26 14:39:32 +0530

    Added time and space complexities

--- Searching/binarySearch.js
+++ Searching/binarySearch.js
@@ -23,3 +23,6 @@ console.log(binarySearch([1,2,3,4,5], 6));  //-1
 console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 10));  //2
 console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 95));  //16
 console.log(binarySearch([5,6,10,13,14,18,30,34,35,37,40,44,64,79,84,86,95,96,98,99], 100));  //-1
+
+// Time Complexity - O(logn)
+// Space Complexity - O(1)
